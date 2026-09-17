725aa42fdcee29a7c2fefb2e468fc84c6c2a17a5
Полина Тетерина <polinateterinansk@gmail.com>
2026-09-17 10:28:44 +0700

    Добавьте файлы проекта.

---
+++ BusinessLogic/Logic.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+namespace BusinessLogic
+{
+    public class Logic
+    {
+        private readonly List<Student> students = new();
+        public void AddStudent(string fullName, string group, string direction)
+        {
+            students.Add(new Student
+            {
+                FullName = fullName,
+                Group = group,
+                Direction = direction
+            });
+        }
+    }
+}
---
+++ Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Student
+    {
+        public string FullName { get; set; } 
+        public string Group { get; set; } 
+        public string Direction { get; set; }
+    }
+}
