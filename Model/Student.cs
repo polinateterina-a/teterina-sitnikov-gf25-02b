02ae23ee7665788f3f5d3417fa4c66d3c79e0ecc
SitnikovArtem GF25-02B <artioms921@gmail.com>
2026-09-24 00:47:53 +0700

    Изменения в модели

--- Model/Student.cs
+++ Model/Student.cs
@@ -2,8 +2,51 @@
 {
     public class Student
     {
-        public string Name { get; set; } 
-        public string Speciality { get; set; } 
-        public string Group { get; set; }
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    Name = value;
+                }
+            }
+        }
+        private string _speciality;
+        public string Speciality
+        {
+            get
+            {
+                return _speciality;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(_speciality))
+                {
+                    Speciality = value;
+                }
+            }
+        }
+
+        private string _group;
+        public string Group
+        {
+            get
+            {
+                return _group;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(_group))
+                {
+                    Group = value;
+                }
+            }
+        }
     }
 }
